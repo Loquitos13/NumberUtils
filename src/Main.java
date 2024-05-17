2628646ca321fc01a578005d634343404ee74bd3
Loquitos13 <dbp13@ua.pt>
2024-05-17 11:26:55 +0100

    ficheiro Main todo comentado com opcao de ordenacao da sequencia de numeros

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Double> numeros = new ArrayList<>();
-
-        System.out.println("Insira os números (termine com um ponto final):");
-
-        // Lê os números do utilizador até encontrar um ponto final
+        System.out.println("Insira os números (para terminar, inserir um ponto final):");
+        // le os números do utilizador até encontrar um ponto final
         while (true) {
             String input = scanner.nextLine().trim();
             if (input.equals(".")) {
@@ -20,49 +18,76 @@ public class Main {
                 System.out.println("Entrada inválida. Por favor, insira uma sequência de números ou um ponto final caso pretenda terminar.");
             }
         }
-
-        // Verifica se a listade números está vazia
+        // verifica se a listade números está vazia
         if (numeros.isEmpty()) {
             System.out.println("Nenhum número foi inserido.");
             return;
         }
-
-        // Calcula a média
+        // calcula a média
         double media = NumberUtils.calcularMedia(numeros);
         System.out.println("Média: " + media);
 
-        // Calcula a mediana
+        // calcula a mediana (numero do meio)
         double mediana = NumberUtils.calcularMediana(numeros);
         System.out.println("Mediana: " + mediana);
 
-        // Obtém a moda
+        // obtém a moda
         ModaResultado modaResultado = NumberUtils.calcularModa(numeros);
         List<Double> moda = ModaResultado.getModa();
-        if (moda.isEmpty()) {
+        if (moda.isEmpty()) { //se cada número aparecer apenas 1x
             System.out.println("Moda: Não existe. Cada elemento aparece apenas 1 vez.");
-        } else {
+        } else { //o numero que aparece mais vezes é a moda, caso apareça mais que 1x
             System.out.println("Moda: " + moda + " - " + modaResultado.getFreqMaxima() + "x");
         }
-
-
-        // Ordena a lista em ordem crescente
+        // ordena a lista em ordem crescente ou decrescente
         List<Double> numerosCrescente = new ArrayList<>(numeros);
-        System.out.println("Pretende obter a ordenação da sequência de números por ordem Crescente (1,2,3,...) ou Decrescente (10,9,8,...) ?");
+    // opção de ordenar por ordem crescente,decrescente ou ambas
+        System.out.println("Pretende obter a ordenação da sequência de números por ordem Crescente (1,2,3,...), Decrescente (10,9,8,...) ou Ambas (Crescente e Decrescente) ?");
         Scanner scannerCrescente = new Scanner(System.in);
         String ordenacao = scannerCrescente.nextLine();
 
-        if (Objects.equals(ordenacao, "crescente") || Objects.equals(ordenacao, "Crescente")|| Objects.equals(ordenacao, "CRESCENTE")) {
+        if (Objects.equals(ordenacao, "crescente") || Objects.equals(ordenacao, "Crescente") || Objects.equals(ordenacao, "CRESCENTE")) {
             NumberUtils.ordenarLista(numerosCrescente, true);
             System.out.println("Lista em ordem crescente: " + numerosCrescente);
-        } else {
-            // Ordena a lista em ordem decrescente
+        } else if (Objects.equals(ordenacao, "ambas") || Objects.equals(ordenacao, "AMBAS") || Objects.equals(ordenacao, "Ambas")) {
+            NumberUtils.ordenarLista(numerosCrescente, true);
+            System.out.println("Lista em ordem Crescente: " + numerosCrescente);
+            NumberUtils.ordenarLista(numerosCrescente, false);
+            System.out.println("Lista em ordem Decrescente: " + numerosCrescente);
+        } else if (Objects.equals(ordenacao, "decrescente") || Objects.equals(ordenacao, "DECRESCENTE") || Objects.equals(ordenacao, "Decrescente")) {
+            // ordena a lista em ordem decrescente
             List<Double> numerosDecrescente = new ArrayList<>(numeros);
             NumberUtils.ordenarLista(numerosDecrescente, false);
             System.out.println("Lista em ordem decrescente: " + numerosDecrescente);
+        } else {
+            String opcao; //nova variavel para obter a opcao de ordenacao em caso de texto incompativel com opcoes definidas
+            do {
+                //caso o utilizador insira um texto que nao corresponda às opções
+                System.out.println("Opção Inválida.");
+                //output de ajuda
+                System.out.println("Escrever:\n - 'Crescente' para obter a lista por ordem crescente (1,2,3,...);\n - 'Decrescente' para obter a lista por ordem decrescente (10,9,8,...)\n - 'Ambas' para obter ambas as ordenações (Crescente e Decrescente).");
+                Scanner scannerLoop = new Scanner(System.in);
+                opcao = scannerLoop.nextLine();
+                //o loop pede uma nova inserção de texto ao utilizador e verifica se corresponde a alguma da opções disponíveis
+                //caso seja diferente, volta a pedir a inserção de texto até que corresponda
+            } while (!Objects.equals(opcao, "decrescente") && !Objects.equals(opcao, "DECRESCENTE") && !Objects.equals(opcao, "Decrescente") &&
+                    !Objects.equals(opcao, "CRESCENTE") && !Objects.equals(opcao, "Crescente") && !Objects.equals(opcao, "crescente") &&
+                    !Objects.equals(opcao, "ambas") && !Objects.equals(opcao, "AMBAS") && !Objects.equals(opcao, "Ambas"));
 
-            scanner.close();
+            // repete o mesmo processo apenas quando for inserida uma opção válida
+            if (Objects.equals(opcao, "crescente") || Objects.equals(opcao, "Crescente") || Objects.equals(opcao, "CRESCENTE")) {
+                NumberUtils.ordenarLista(numerosCrescente, true);
+                System.out.println("Lista em ordem crescente: " + numerosCrescente);
+            } else if (Objects.equals(opcao, "ambas") || Objects.equals(opcao, "AMBAS") || Objects.equals(opcao, "Ambas")) {
+                NumberUtils.ordenarLista(numerosCrescente, true);
+                System.out.println("Lista em ordem Crescente: " + numerosCrescente);
+                NumberUtils.ordenarLista(numerosCrescente, false);
+                System.out.println("Lista em ordem Decrescente: " + numerosCrescente);
+            } else if (Objects.equals(opcao, "decrescente") || Objects.equals(opcao, "DECRESCENTE") || Objects.equals(opcao, "Decrescente")) {
+                List<Double> numerosDecrescente = new ArrayList<>(numeros);
+                NumberUtils.ordenarLista(numerosDecrescente, false);
+                System.out.println("Lista em ordem decrescente: " + numerosDecrescente);
+            }
         }
-
-
     }
 }
